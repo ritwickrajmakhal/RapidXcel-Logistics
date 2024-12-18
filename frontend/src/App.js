8883034a37f58ea6afad36de0224488301852acc
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-12-18 19:33:29 +0530

    Add Navbar to App component and update Dashboard layout for improved responsiveness

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+    <Navbar />
       <Routes>
         <Route path="/dashboard/*" element={<Dashboard />} />
       </Routes>
