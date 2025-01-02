f989c177da91cbcc6f51218a271c2c07e46cd946
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2025-01-02 17:36:17 +0530

    Add Home and Logout routes to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
-import Navbar from './components/Navbar';
 import { ToastContainer } from 'react-toastify';
+import Home from './pages/Home/Home';
+import Logout from './components/Logout';
 
 function App() {
   return (
     <>
-    <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/dashboard/*" element={<Dashboard />} />
+        {/* Logout Route */}
+        <Route path="logout" element={<Logout />} />
       </Routes>
       <ToastContainer position='bottom-right'/>
     </>
