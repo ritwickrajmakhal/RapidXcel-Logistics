81503cceacc0894789833ab94960c89086739a15
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2025-01-22 17:48:04 +0530

    Refactor import statements in conftest.py for improved readability

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,10 +1,8 @@
-import pytest
 import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import pytest
 from rapidxcel_logistics import create_app, db
-
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..')))
 
 @pytest.fixture
 def app():
