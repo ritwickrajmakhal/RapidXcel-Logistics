748e2b7dc434104a08a9a09d6b323765614973f9
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2025-01-22 23:36:50 +0530

    Add ResetPassword route to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,19 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import { ToastContainer } from 'react-toastify';
 import Home from './pages/Home/Home';
 import Logout from './components/Logout';
+import ResetPassword from './pages/Home/ResetPassword';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/reset-password/:resetToken" element={<ResetPassword />} />
         <Route path="/dashboard/*" element={<Dashboard />} />
         {/* Logout Route */}
         <Route path="logout" element={<Logout />} />
       </Routes>
-      <ToastContainer position='bottom-right'/>
+      <ToastContainer position='bottom-right' />
     </>
   );
 }
