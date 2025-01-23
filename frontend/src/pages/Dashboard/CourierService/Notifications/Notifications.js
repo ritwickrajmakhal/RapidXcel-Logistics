0891d76fa1474e88d5e49fec49405e1944f18031
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2025-01-24 02:07:39 +0530

    Refactor notifications component to accept props and reverse notification order; update date formatting and notification styles

--- frontend/src/pages/Dashboard/CourierService/Notifications/Notifications.js
+++ frontend/src/pages/Dashboard/CourierService/Notifications/Notifications.js
@@ -1,40 +1,24 @@
 import "./style.css";
-import { useState } from "react";
 
-const Notifications = () => {
-    const [notifications, setNotifications] = useState([
-        {
-            id: 1,
-            title: "In Transit",
-            description: "Your shipment is currently in transit.",
-            created_at: "2021-09-01T12:00:00Z",
-        },
-        {
-            id: 2,
-            title: "In Transit",
-            description: "Your shipment is currently in transit.",
-            created_at: "2021-09-01T12:00:00Z",
-        },
-        {
-            id: 3,
-            title: "In Transit",
-            description: "Your shipment is currently in transit.",
-            created_at: "2021-09-01T12:00:00Z",
-        },
-    ]);
+const Notifications = ({ notifications }) => {
+    const reversedNotifications = [...notifications].reverse();
 
+    const formatDate = (dateString) => {
+        const options = { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit' };
+        return new Date(dateString).toLocaleString('en-GB', options).replace(',', '');
+    };
     return (
         <section className="notifications-section">
             <h2>Notifications</h2>
             <div id="notificationList" className="notifications">
-                {notifications.map((notification) => (
-                    <div className="notification transit" key={notification.id}>
+                {reversedNotifications.map((notification) => (
+                    <div className={`notification ${notification.title.toLowerCase()}`} key={notification.id}>
                         <div className="details">
                             <div>
                                 <h3>{notification.title}</h3>
-                                <p>{notification.description}</p>
+                                <p>{notification.message}</p>
                             </div>
-                            <span className="status">{notification.created_at}</span>
+                            <span className="status">{formatDate(notification.created_at)}</span>
                         </div>
                     </div>
                 ))}
